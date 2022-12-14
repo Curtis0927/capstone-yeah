2cbf5d555d3eddefadfc019f46ebc98c6a74851f
Curtis0927 <curtis0927@gmail.com>
2022-12-13 22:54:33 -0800

    some new learnin

---
+++ SOmething.py
@@ -0,0 +1,14 @@
+""" random triangle to show how python works in order
+print("     ()")
+print("    (  )")
+print("   (    )")
+print("  (      )")
+print(" (        )")
+print("(__________)")
+"""
+#substituting things with variables
+person_name = "Bob"
+person_age ="271"
+print ("bros name is " + person_name)
+print ("bros age is also " + person_age)
+
