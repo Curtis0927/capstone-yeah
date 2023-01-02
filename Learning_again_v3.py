cbe0d804f804342ec144c0e991cff66bdb34e317
Curtis0927 <curtis0927@gmail.com>
2023-01-01 21:52:15 -0800

    learning more python! again

---
+++ Learning_again_v3.py
@@ -0,0 +1,20 @@
+# print("yo")
+# print("WASD" * 10)
+
+# name = "John smith"
+# age = 20
+# New_patient = True
+
+
+# name = input("what is your name:")
+# color = input("what is your favorite color:")
+# print(name + "likes" + color)
+
+# ask_weight = input("what is thy weight in pounds: ")
+
+# print (int(ask_weight) * float(0.45359237))
+
+ayo = "heyoo"
+print(ayo[-2])
+print(ayo[0:4])
+print(ayo[1:])
